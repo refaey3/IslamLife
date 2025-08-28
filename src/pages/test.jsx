6b6913ab898ef58b4e5921d27c482a216b6cfe9c
Mostafa ElRefaey <mostafa.elrefaey3@gmail.com>
2025-08-28 03:30:05 +0300

    no

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-export default function Quran() {
-  return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <iframe
-        src="/standard1-quran.pdf"
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-        title="Quran PDF"
-      ></iframe>
-    </div>
-  );
-}
+// export default function Quran() {
+//   return (
+//     <div style={{ width: "100%", height: "100vh" }}>
+//       <iframe
+//         src="/standard1-quran.pdf"
+//         width="100%"
+//         height="100%"
+//         style={{ border: "none" }}
+//         title="Quran PDF"
+//       ></iframe>
+//     </div>
+//   );
+// }
