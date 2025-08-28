1e3e6764dcf4e11e950e4da50ad3da07a64572c9
Mostafa ElRefaey <mostafa.elrefaey3@gmail.com>
2025-08-28 18:27:56 +0300

    issue

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,15 +1,7 @@
-// import React from "react";
+import React from 'react'
 
-// export default function Quran() {
-//   return (
-//     <div style={{ width: "100%", height: "100vh" }}>
-//       <iframe
-//         src="/standard1-quran.pdf"
-//         width="100%"
-//         height="100%"
-//         style={{ border: "none" }}
-//         title="Quran PDF"
-//       ></iframe>
-//     </div>
-//   );
-// }
+export default function test() {
+  return (
+    <div>test</div>
+  )
+}
