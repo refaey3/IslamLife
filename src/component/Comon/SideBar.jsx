f0be2b70f81728b19a24bb4909f822f00a1fc574
Mostafa ElRefaey <mostafa.elrefaey3@gmail.com>
2025-08-28 03:06:25 +0300

    quran

--- src/component/Comon/SideBar.jsx
+++ src/component/Comon/SideBar.jsx
@@ -17,9 +17,9 @@ const SideBarContainer = styled.div`
   position: fixed;
   top: 50px;
   right: ${({ isOpen }) => (isOpen ? "0" : "-260px")};
-  width: 170px;
+  width: 173px;
   height: calc(100vh - 51px);
-  background: rgba(107, 112, 92, 0.9); /* لون خلفية مشابه للتصميم */
+  background: rgba(107, 112, 92, 0.9); 
   border: 1px solid rgba(255, 255, 255, 0.2);
   backdrop-filter: blur(10px);
   box-shadow: -2px 0 8px rgba(0, 0, 0, 0.2);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeroSection from "../component/Home/HeroSection";
 import RecitersList from "../component/Reciters/RecitersList";
 import PrayerTimes from "../component/Prayer/PrayerTimes";
+import Haddith from "../component/Prayer/Haddith";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <PrayerTimes />
+      <Haddith />
       <RecitersList />
     </>
   );
